936f65c903e5b6320f8ab11b858963b5ba1eb50e
Dillon Teakell <teakelld@icloud.com>
2024-05-17 13:10:15 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dillon Teakell on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
