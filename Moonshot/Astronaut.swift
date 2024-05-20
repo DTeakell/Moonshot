df0f6687ad2eb5b22748bc4de8b2eeb662e3c745
Dillon Teakell <teakelld@icloud.com>
2024-05-19 23:55:33 -0400

    Begin work on Moonshot project

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,8 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Dillon Teakell on 5/19/24.
+//
+
+import Foundation
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Dillon Teakell on 5/19/24.
+//
+
+import Foundation
